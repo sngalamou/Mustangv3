c39d7637c86c3964ba499317a45b0197f8f63031
sngalamou <42877564+sngalamou@users.noreply.github.com>
2019-11-25 15:40:30 -0600

    testing

--- save-contacts.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $contacts = $_POST["contacts"];
-    $myfile = fopen("contacts.json", "w") or die("Unable to open file to write!");
-    fwrite($myfile, $contacts);
-    fclose($myfile);
-    echo "Contacts saved";
-?>
